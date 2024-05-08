f67ec34e5a7bdca6ad187d03aef571f9dc6fa612
Piyush Dhyani <piyushdhyani7@gmail.com>
2024-05-08 08:30:44 +0530

    less see

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 const connectToMongo = require('./db');
-const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 5000;
-
+const cors = require("cors")
 connectToMongo();
 
 app.use(cors({
-  origin: "https://digitalised-hospital-system-frontend.vercel.app",
-  methods: ["GET", "POST", "PUT"],
-  allowedHeaders: [
-    "Origin",
-    "Content-Type",
-    "Accept",
-    "Authorization",
-    "X-Request-With",
-    "multipart/form-data"
-  ],
-  credentials: true
-}));
+  origin: "*",
+}))
 
 app.use(express.json());
-
-// Set CORS headers for all responses
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://digitalised-hospital-system-frontend.vercel.app/');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With, multipart/form-data');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/list', require('./routes/list'));
